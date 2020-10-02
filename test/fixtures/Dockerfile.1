3df3646ab90136ccf1897a44d407fe5a9335df61
Nicolas Martinez <17040442+nicomt@users.noreply.github.com>
2020-10-02 02:34:17 -0400

    Improving run test  - Allowing to run pull tests in parallel  - Mobing log to module scope

---
+++ test/fixtures/Dockerfile.1
@@ -0,0 +1,3 @@
+FROM busybox
+
+COPY test1 /test
--- test/tasks/run.js
+++ test/tasks/run.js
@@ -4,12 +4,13 @@ const util = require('../docker-util');
 const MockLog = require('../mock/log');
 const RunTask = require('../../lib/tasks/run');
 
+const log = new MockLog();
+
 async function sleep(ms) {
   return new Promise(res => setTimeout(res, ms));
 }
 
 test('run: simple', async (t) => {
-  const log = new MockLog();
   const task = new RunTask('test', {
     image: 'busybox',
     command: 'echo "hello world"'
@@ -23,7 +24,6 @@ test('run: simple', async (t) => {
 
 
 test('run: environment', async (t) => {
-  const log = new MockLog();
   const task = new RunTask('test', {
     image: 'busybox',
     environment: {
@@ -39,23 +39,21 @@ test('run: environment', async (t) => {
 
 
 test('run: mount', async (t) => {
-  const log = new MockLog();
   const task = new RunTask('test', {
     image: 'busybox',
     volumes: [
-      `${path.join(path.resolve(__dirname, '..'), 'fixtures/test')}:/test`
+      `${path.join(path.resolve(__dirname, '..'), 'fixtures/test1')}:/test`
     ],
     command: 'cat /test'
   });
 
   const { exitCode, output } = await task.execute(log);
   t.is(exitCode, 0);
-  t.is(output, 'hello world');
+  t.is(output, 'test1');
 });
 
 
 test('run: auto remove enabled', async (t) => {
-  const log = new MockLog();
   const task = new RunTask('test', {
     image: 'busybox',
     command: 'echo "hello world"',
@@ -67,8 +65,8 @@ test('run: auto remove enabled', async (t) => {
   t.false(await util.containerExists(containerId), `Container ${containerId} must be deleted when auto_remove is true`);
 });
 
+
 test('run: auto remove disabled', async (t) => {
-  const log = new MockLog();
   const task = new RunTask('test', {
     image: 'busybox',
     command: 'echo "hello world"',
@@ -80,77 +78,67 @@ test('run: auto remove disabled', async (t) => {
   t.true(await util.containerExists(containerId), `Container ${containerId} should not be deleted when auto_remove is false`);
 });
 
-// Running all pull test in sequentially to avoid interference with the hello-world image
-test('run: pull', async (t) => {
-  const log = new MockLog();
 
+test('run: pull missing', async (t) => {
+  const task = new RunTask('test', {
+    image: 'nicomt/test:test1',
+    command: 'echo test',
+    pull: 'missing'
+  });
+
+  await util.removeImage('nicomt/test:test1');
+
+  const start1 = Date.now();
+  const { exitCode: ec1 } = await task.execute(log);
+  t.is(ec1, 0);
+  t.true(task.lastPull > start1, 'Image not pulled');
+
+  const start2 = Date.now();
+  const { exitCode: ec2 } = await task.execute(log);
+  t.is(ec2, 0);
+  t.false(task.lastPull > start2, 'Image pulled even-though image exists');
+});
+
+
+test('run: pull never', async (t) => {
+  const task = new RunTask('test', {
+    image: 'nicomt/test:test2',
+    command: 'echo test',
+    pull: 'never'
+  });
+
+  await util.removeImage('nicomt/test:test2');
   try {
-    // #### Pull Missing ####
-    await (async () => {
-      const task = new RunTask('test', {
-        image: 'hello-world',
-        pull: 'missing'
-      });
+    await task.execute(log);
+    t.fail('Task should fail if no image available and pull never');
+    // eslint-disable-next-line no-empty
+  } catch (err) {}
 
-      await util.removeImage('hello-world');
+  t.is(task.lastPull, 0);
 
-      const start1 = Date.now();
-      const { exitCode: ec1 } = await task.execute(log);
-      t.is(ec1, 0);
-      t.true(task.lastPull > start1, 'Image not pulled');
+  await util.pullImage('nicomt/test:test2');
+  const { exitCode } = await task.execute(log);
+  t.is(exitCode, 0);
+  t.is(task.lastPull, 0);
+});
 
-      const start2 = Date.now();
-      const { exitCode: ec2 } = await task.execute(log);
-      t.is(ec2, 0);
-      t.false(task.lastPull > start2, 'Image pulled even-though image exists');
-    })();
+test('run: pull', async (t) => {
+  const task = new RunTask('test', {
+    image: 'nicomt/test:test3',
+    command: 'echo test',
+    pull: 'always'
+  });
 
+  await util.removeImage('nicomt/test:test3');
+  const start1 = Date.now();
+  const { exitCode: ec1 } = await task.execute(log);
+  t.is(ec1, 0);
+  t.true(task.lastPull > start1, 'Image should always be pulled');
 
-    // #### Pull Never ####
-    await (async () => {
-      const task = new RunTask('test', {
-        image: 'hello-world',
-        pull: 'never'
-      });
-
-      await util.removeImage('hello-world');
-      try {
-        await task.execute(log);
-        t.fail('Task should fail if no image available and pull never');
-        // eslint-disable-next-line no-empty
-      } catch (err) {}
-
-      t.is(task.lastPull, 0);
-
-      await util.pullImage('hello-world');
-      const { exitCode } = await task.execute(log);
-      t.is(exitCode, 0);
-      t.is(task.lastPull, 0);
-    })();
-
-    // #### Pull Always ####
-    await (async () => {
-      const task = new RunTask('test', {
-        image: 'hello-world',
-        pull: 'always'
-      });
-
-      await util.removeImage('hello-world');
-      const start1 = Date.now();
-      const { exitCode: ec1 } = await task.execute(log);
-      t.is(ec1, 0);
-      t.true(task.lastPull > start1, 'Image should always be pulled');
-
-      const start2 = Date.now();
-      const { exitCode: ec2 } = await task.execute(log);
-      t.is(ec2, 0);
-      t.true(task.lastPull > start2, 'Image should always be pulled');
-    })();
-
-  } finally {
-    await util.removeImage('hello-world');
-  }
-
+  const start2 = Date.now();
+  const { exitCode: ec2 } = await task.execute(log);
+  t.is(ec2, 0);
+  t.true(task.lastPull > start2, 'Image should always be pulled');
 });
 
 
