d70fb0408ed801df9e62515439636411edbaadc1
Nicolas Martinez <17040442+nicomt@users.noreply.github.com>
2020-10-02 02:00:01 -0400

    Better CLI logging and exit behaviour

--- bin/cli.js
+++ bin/cli.js
@@ -16,16 +16,18 @@ program
       await scheduler.loadConfig(cmd.config);
       scheduler.start();
     } catch (err) {
-      console.error(err.message || err);
-      process.exitCode = 1;
+      process.stderr.write(`${err.message || err.toString()}\n`, () => {
+        process.exit(1);
+      });
     }
   });
 
 program.parse(process.argv);
 
 process.on('SIGINT', () => {
-  console.info('Interrupted');
-  process.exit(0);
+  process.stdout.write('Interrupted\n', () => {
+    process.exit(0);
+  });
 });
 
 
