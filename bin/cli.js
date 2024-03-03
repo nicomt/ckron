f528a7ec9ac26ccf11910c758166dbcb8d2309cd
Nicolas Martinez <nicolas.martinez@morgansolar.com>
2024-03-02 22:54:52 -0500

    CLI Improvements: - New command to run job - Refactor CLI and error handling

--- bin/cli.js
+++ bin/cli.js
@@ -2,47 +2,57 @@
 
 import fs from 'fs';
 import { Command } from 'commander';
-import betterAjvErrors from 'better-ajv-errors';
 import Ckron from '../lib/ckron.js';
+import CkronError from '../lib/error.js';
 
 const { version } = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url)));
 const program = new Command();
 
+function handleError(err) {
+  if (err instanceof CkronError) {
+    program.error(err.message, { exit: err.exitCode });
+  }
+  throw err;
+}
+
 program
-  .version(version)
+  .name('ckron')
+  .option('--config [path]', 'configuration file', '/etc/ckron/config.yml')
+  .description('Cron-like job scheduler for docker')
+  .version(version);
+
+program
   .command('daemon')
-  .option('--config <path>', 'configuration file', '/etc/ckron/config.yml')
-  .action(async (cmd) => {
+  .description('Run the scheduler daemon')
+  .action(async (_, cmd) => {
+    const { config } = cmd.optsWithGlobals();
     try {
       const scheduler = new Ckron();
-      await scheduler.loadConfig(cmd.config);
+      await scheduler.loadConfig(config);
       scheduler.start();
     } catch (err) {
-      if (err.code === 'CONFIG_SYNTAX') {
-        const output = betterAjvErrors(
-          err.validationSchema,
-          err.validationData,
-          [err.validationErrors[0]],
-          { indent: 2 }
-        );
-        process.stderr.write(output, () => {
-          process.exit(1);
-        });
-      } else {
-        process.stderr.write(`${err.message || err.toString()}\n`, () => {
-          process.exit(1);
-        });
-      }
+      handleError(err);
     }
   });
 
-program.parse(process.argv);
-
-process.on('SIGINT', () => {
-  process.stdout.write('Interrupted\n', () => {
-    process.exit(0);
+program
+  .command('run <job>')
+  .description('Run a job')
+  .option('--notify', 'send notification on error', false)
+  .action(async (_, cmd) => {
+    const { config, job, notify } = cmd.optsWithGlobals();
+    try {
+      const scheduler = new Ckron();
+      await scheduler.loadConfig(config);
+      await scheduler.runJob(job, notify);
+    } catch (err) {
+      handleError(err);
+    }
   });
-});
+
+
+program.parse();
+
 
 
 
